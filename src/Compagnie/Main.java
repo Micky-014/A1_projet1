cbd2b92d010793c206808aa843aed38a98d43087
Micky <micky.harau@gmail.com>
2024-12-03 11:48:02 +0100

    Version 4

--- src/Compagnie/Main.java
+++ src/Compagnie/Main.java
@@ -6,8 +6,26 @@ public class Main {
     public static void main(String[] args) {
         Personne personne = new Personne("Micky", "Paris", "0611111111");
         personne.ObtenirInfos();
-        Pilote pilote = new Pilote( 1,  5000,1,"01/04/2004", "Theotim", "Paris", "0622222222");
+        Passager passager = new Passager("Français",personne.getPrenom(), personne.getAdresse(), personne.getContact());
+        Pilote pilote = new Pilote( 1,  5000,1,"12/05/2004", "Maxime", "Paris", "0622222222");
         pilote.ObtenirInfos();
-        pilote.ObtenirRole(2);
+        pilote.ObtenirRole(1);
+        PersonnelCabine personnelCabine = new PersonnelCabine("Hotesse",2,"15/10/2015","Nastassia","Paris", "0633333333");
+        personnelCabine.ObtenirInfos();
+        personnelCabine.ObtenirRole(2);
+        Vol vol = new Vol(1,"Paris","Shanghai","0","0");
+        passager.reserverVol(vol);
+        passager.obtenirReservation(1);
+        Reservation reservation =passager.obtenirReservation(1);
+        reservation.confirmeReservation(1);
+        reservation.getStatut();
+        passager.annulerReservation(1);
+        reservation.getStatut();
+        personnelCabine.affecterVol(vol);
+        personnelCabine.obtenirVol(vol.getNumeroVol());
+        pilote.affecterVol(vol);
+        pilote.obtenirVol(vol.getNumeroVol());
+        vol.getEquipageCabine();
+        vol.getPilote();
     }
 }
--- src/Compagnie/Passager.java
+++ src/Compagnie/Passager.java
@@ -4,11 +4,13 @@ import java.util.HashMap;
 
 public class Passager extends Personne{
     private String passeport;
-    private static HashMap<Integer,Vol> volReserve;
-    private static HashMap<Integer,Reservation> listeReservation;
-    public Passager(int numeroEmploye, String dateEmbauche, String prenom, String adresse, String contact){
+    private HashMap<Integer,Vol> volReserve;
+    private HashMap<Integer,Reservation> listeReservation;
+    public Passager(String passeport, String prenom, String adresse, String contact){
         super(prenom, adresse, contact);
         this.passeport = passeport;
+        this.volReserve = new HashMap<>();
+        this.listeReservation = new HashMap<>();
     }
     public String getPasseport() {
         return passeport;
@@ -21,8 +23,10 @@ public class Passager extends Personne{
         System.out.println("Passeport: " + passeport);
     }
     public void reserverVol(Vol vol){
+        System.out.println(vol.getNumeroVol());
         volReserve.put(vol.getNumeroVol(),vol);
         Reservation reservation = new Reservation();
+        listeReservation.put(reservation.getNumeroReservation(),reservation);
         reservation.getVol(vol);
         reservation.addResVol(vol);
     }
@@ -39,8 +43,10 @@ public class Passager extends Personne{
         Reservation reservation = listeReservation.get(idRes);
         reservation.setStatut("Annulée");
     }
-    public void obtenirReservation(int idRes){
+    public Reservation obtenirReservation(int idRes){
+        System.out.println(listeReservation);
         Reservation reservation = listeReservation.get(idRes);
         reservation.getInfos();
+        return reservation;
     }
 }
